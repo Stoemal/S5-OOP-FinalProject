fc6075f023d46f41b54be9afe76ba6ff54cd97dd
QuentinK <quentinkaufman2000@gmail.com>
2021-01-21 17:08:08 +0100

    Added class : Employee, Officer, Deliverydriver

---
+++ DeliveryDriver.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S5_OOP_FinalProject
+{
+    sealed class DeliveryDriver : Employee
+    {
+        private string meanOfTransport;
+        private int orderCount;
+        public DeliveryDriver(string firstName, string lastName, string address, string phoneNumber, string position, string meanOfTransport, int orderCount) : base (firstName, lastName, address, phoneNumber, position)
+        {
+            this.meanOfTransport = meanOfTransport;
+            this.orderCount = orderCount;
+        }
+
+
+    }
+}
---
+++ Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S5_OOP_FinalProject
+{
+    class Employee : Person
+    {
+        protected string position;
+        public Employee(string firstName, string lastName, string address, string phoneNumber, string position) : base (firstName, lastName, address, phoneNumber)
+        {
+            this.position = position;
+        }
+
+
+
+    }
+}
