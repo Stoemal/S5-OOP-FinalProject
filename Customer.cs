d5bcb6ae54081dee058832b02f26f3bf6c8924cf
IdhemAK <mehdi.ben_maamar@edu.devinci.fr>
2021-01-21 16:43:05 +0100

    Classe Person ajoutée

---
+++ Customer.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public class Customer : 
+{
+
+	public Customer()
+	{
+
+	}
+}
---
+++ Person.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+abstract public class Person
+{
+    protected string firstName;
+    protected string lastName;
+    protected string address;
+    protected string phoneNumber;
+
+	public Person(string firstName, string lastName, string address, string phoneNumber)
+	{
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+	}
+
+    #region Accesseurs
+    public string FirstName
+    {
+        get { return this.firstName; }
+        set { this.firstName = value; }
+    }
+
+    public string LastName
+    {
+        get { return this.lastName; }
+        set { this.lastName = value; }
+    }
+
+    public string Address
+    {
+        get { return this.address; }
+        set { this.address = value; }
+    }
+
+    public string PhoneNumber
+    {
+        get { return this.phoneNumber; }
+        set { this.phoneNumber = value; }
+    }
+
+    #endregion Accesseurs
+
+    public override string ToString()
+    {
+        return "Nom : " + lastName + "\nPrénom : " + firstName + "\nAdresse : " + address + "\nTéléphone : " + phoneNumber;
+    }
+}
