804c4e19eb2339c3fa59c3dd44d9031eb0eaf0b9
QuentinK <quentinkaufman2000@gmail.com>
2021-01-21 17:23:22 +0100

    Added Pizzeria class

---
+++ Pizzeria.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S5_OOP_FinalProject
+{
+    class Pizzeria
+    {
+        private List<Employee> listEmployee;
+        private List<Customer> listCustomer;
+        private float dayResult;
+    }
+}
