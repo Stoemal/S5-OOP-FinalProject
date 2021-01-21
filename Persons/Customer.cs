6532bad27d20505badfb89e35e6b2a3afa7f5eb1
QuentinK <quentinkaufman2000@gmail.com>
2021-01-21 18:09:55 +0100

    Added Customer class

---
+++ Persons/Customer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S5_OOP_FinalProject
+{
+    public sealed class Customer : Person
+    {
+        private DateTime firstOrder;
+        private List<Order> listOrder;
+        private float cumulativeOrder;
+        public Customer(string firstName, string lastName, string address, string phoneNumber, DateTime firstOrder, List<Order> listOrder, float cumulativeOrder) : base(firstName, lastName, address, phoneNumber)
+        {
+            this.firstOrder = firstOrder;
+            this.listOrder = listOrder;
+            this.cumulativeOrder = cumulativeOrder;
+        }
+    }
+}
